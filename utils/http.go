2e7694a2d6ca9d42b871faa600d60433cd523167
aryanmalik2110 <aryan@vizares.com>
2025-02-19 00:36:32 +0530

    bug fix

--- utils/http.go
+++ utils/http.go
@@ -42,7 +42,7 @@ func (f *StaticFileInfoWrapper) ModTime() time.Time {
 
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
