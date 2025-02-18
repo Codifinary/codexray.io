4a89e29bed1540ada97b4c355c41deb5bdd1ae24
aryanmalik2110 <aryan@vizares.com>
2025-02-18 21:03:24 +0530

    cors fix

--- utils/http.go
+++ utils/http.go
@@ -7,8 +7,6 @@ import (
 	"time"
 )
 
-const FRONTE_END_BRUM = "http://34.47.146.55:3000"
-
 type StaticFSWrapper struct {
 	http.FileSystem
 	modTime time.Time
@@ -47,7 +45,6 @@ func EnableCORS(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
