72426becf8c2ce53bb5a128217c62e323e42bb2e
Saha-Shibani <sahashibani57@gmail.com>
2025-05-06 15:10:52 +0530

    added structs for dashBoard

--- api/views/overview/dashboard.go
+++ api/views/overview/dashboard.go
@@ -11,13 +11,50 @@ import (
 )
 
 type DashboardView struct {
-	Status    model.Status  `json:"status"`
-	Message   string        `json:"message"`
-	EumApps   []EumOverview `json:"eumOverview"`
-	BadgeView EumBadge      `json:"badgeView"`
+	Status       model.Status        `json:"status"`
+	Message      string              `json:"message"`
+	Applications ApplicationOverview `json:"applications"`
+	EumOverview  EumOverview         `json:"eumOverview"`
+	Nodes        NodeOverview        `json:"nodes"`
+	Incidents    IncidentOverview    `json:"incidents"`
+}
+
+type ApplicationOverview struct {
+	ApplicationStatus ApplicationsStats  `json:"applicationsStats"`
+	Applications      []ApplicationTable `json:"applications"`
 }
 
 type EumOverview struct {
+	EumApps   []EumTable `json:"eumOverview"`
+	BadgeView EumBadge   `json:"badgeView"`
+}
+
+type NodeOverview struct {
+	NodeStats NodeStats    `json:"nodeStats"`
+	Nodes     []NodesTable `json:"nodes"`
+}
+
+type IncidentOverview struct {
+	IncidentStats IncidentStats   `json:"incidentStats"`
+	Incidents     []IncidentTable `json:"incidents"`
+}
+
+type ApplicationTable struct {
+	ID                   string  `json:"id"`
+	Status               string  `json:"status"`
+	TransactionPerSecond float64 `json:"transactionPerSecond"`
+	ResponseTime         float64 `json:"responseTime"`
+	Errors               uint64  `json:"errors"`
+}
+
+type ApplicationsStats struct {
+	Total int64 `json:"total"`
+	Good  int64 `json:"good"`
+	Fair  int64 `json:"fair"`
+	Poor  int64 `json:"poor"`
+}
+
+type EumTable struct {
 	ServiceName       string  `json:"serviceName" ch:"ServiceName"`
 	AppType           string  `json:"appType" ch:"AppType"`
 	RequestsPerSecond float64 `json:"requestsPerSecond" ch:"requestsPerSecond"`
@@ -31,12 +68,44 @@ type EumBadge struct {
 	MobileApps  uint64 `json:"mobileApps" ch:"mobileApps"`
 }
 
+type NodeStats struct {
+	TotalNodes     int64   `json:"totalNodes"`
+	UpNodes        int64   `json:"upNodes"`
+	DownNodes      int64   `json:"downNodes"`
+	AvgCPUUsage    float64 `json:"avgCpuUsage"`
+	AvgMemoryUsage float64 `json:"avgMemoryUsage"`
+	AvgDiskUsage   float64 `json:"avgDiskUsage"`
+}
+
+type NodesTable struct {
+	NodeName    string  `json:"nodeName"`
+	NodeStatus  string  `json:"nodeStatus"`
+	CpuUsage    float64 `json:"cpuUsage"`
+	MemoryUsage float64 `json:"memoryUsage"`
+	DiskUsage   float64 `json:"diskUsage"`
+}
+
+type IncidentStats struct {
+	TotalIncidents    int64 `json:"totalIncidents"`
+	CriticalIncidents int64 `json:"criticalIncidents"`
+	WarningIncidents  int64 `json:"warningIncidents"`
+	ClosedIncidents   int64 `json:"closedIncidents"`
+}
+
+type IncidentTable struct {
+	ApplicationName string    `json:"applicationName"`
+	OpenIncidents   int64     `json:"openIncidents"`
+	LastOccurence   time.Time `json:"lastOccurrence"`
+}
+
 func renderDashboard(ctx context.Context, ch *clickhouse.Client, w *model.World) *DashboardView {
 	v := &DashboardView{}
 
 	from := w.Ctx.From.ToStandard()
 	to := w.Ctx.To.ToStandard()
 
+	// EUM Overview
+	var eumOverview EumOverview
 	eumApps, badge, err := getEumOverviews(ctx, ch, from, to)
 	if err != nil {
 		klog.Errorln(err)
@@ -44,20 +113,20 @@ func renderDashboard(ctx context.Context, ch *clickhouse.Client, w *model.World)
 		v.Message = err.Error()
 		return v
 	}
-
-	v.EumApps = eumApps
-	v.BadgeView = badge
+	eumOverview.EumApps = eumApps
+	eumOverview.BadgeView = badge
+	v.EumOverview = eumOverview
 	v.Status = model.OK
 	return v
 }
 
-func getEumOverviews(ctx context.Context, ch *clickhouse.Client, from, to time.Time) ([]EumOverview, EumBadge, error) {
+func getEumOverviews(ctx context.Context, ch *clickhouse.Client, from, to time.Time) ([]EumTable, EumBadge, error) {
 	rows, err := ch.GetEUMOverview(ctx, &from, &to)
 	if err != nil {
 		return nil, EumBadge{}, err
 	}
 
-	var eumOverviews []EumOverview
+	var eumTable []EumTable
 	durationSeconds := to.Sub(from).Seconds()
 
 	for _, row := range rows {
@@ -66,7 +135,7 @@ func getEumOverviews(ctx context.Context, ch *clickhouse.Client, from, to time.T
 			requestsPerSecond = float64(row.Requests) / durationSeconds
 		}
 
-		eumOverviews = append(eumOverviews, EumOverview{
+		eumTable = append(eumTable, EumTable{
 			ServiceName:       row.ServiceName,
 			AppType:           row.AppType,
 			RequestsPerSecond: requestsPerSecond,
@@ -86,5 +155,5 @@ func getEumOverviews(ctx context.Context, ch *clickhouse.Client, from, to time.T
 		MobileApps:  appCounts.MobileApps,
 	}
 
-	return eumOverviews, badge, nil
+	return eumTable, badge, nil
 }
